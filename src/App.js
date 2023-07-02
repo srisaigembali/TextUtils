275a8fcb93ae48a2fe8a703b9e04a54c268d5f6f
srisaigembali <70004293+srisaigembali@users.noreply.github.com>
2023-07-02 15:55:20 +0530

    finished

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import About from './components/About';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import Alert from './components/Alert';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
+
+  const removeBodyClasses = () => {
+    document.body.classList.remove('bg-light');
+    document.body.classList.remove('bg-dark');
+    document.body.classList.remove('bg-primary');
+    document.body.classList.remove('bg-success');
+    document.body.classList.remove('bg-warning');
+    document.body.classList.remove('bg-danger');
+  };
+
+  const toggleMode = (cls) => {
+    removeBodyClasses();
+    document.body.classList.add('bg-' + cls);
+    // if (mode === 'dark') {
+    //   setMode('light');
+    //   document.body.style.backgroundColor = 'white';
+    //   showAlert('Light Mode is Enabled', 'success');
+    // } else {
+    //   setMode('dark');
+    //   document.body.style.backgroundColor = '#042743';
+    //   showAlert('Dark Mode is Enabled', 'success');
+    // }
+    if (cls === 'light') {
+      document.body.classList.remove('bg-light');
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert('Light Mode is Enabled', 'success');
+      document.title = 'TextUtils - Light Mode';
+    }
+    if (cls === 'dark') {
+      document.body.classList.remove('bg-dark');
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      showAlert('dark Mode is Enabled', 'success');
+      document.title = 'TextUtils - Dark Mode';
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={
+            <TextForm
+              heading="Enter Your Text To Analyse Below"
+              mode={mode}
+              showAlert={showAlert}
+            />
+          }
+        />
+        <Route exact path="/about" element={<About />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/components/TextForm.js
@@ -0,0 +1,242 @@
+import React, { useState } from 'react';
+
+const TextForm = (props) => {
+  const [text, setText] = useState('Enter your text here');
+
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+    localStorage.setItem('textutil', event.target.value);
+  };
+
+  const handleUpClick = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+    localStorage.setItem('textutil', newText);
+    props.showAlert('Converted to Uppercase!', 'success');
+  };
+
+  const handleLowClick = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+    localStorage.setItem('textutil', newText);
+    props.showAlert('Converted to Lowercase!', 'success');
+  };
+
+  const handleCapitalize = () => {
+    let words = text.split(' ');
+    let newWords = words.map((word) => {
+      return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+    });
+    let newText = newWords.join(' ');
+    setText(newText);
+    localStorage.setItem('textutil', newText);
+    props.showAlert('Capitalized successfully!', 'success');
+  };
+
+  const handleToggleCase = () => {
+    let words = text.split(' ');
+    let newText = words
+      .map((word) => {
+        let newWord = '';
+        for (let i = 0; i < word.length; i++) {
+          let char = word.charAt(i);
+          if (char >= 'A' && char <= 'Z') {
+            char = char.toLowerCase();
+          } else if (char >= 'a' && char <= 'z') {
+            char = char.toUpperCase();
+          }
+          newWord += char;
+        }
+        return newWord;
+      })
+      .join(' ');
+
+    setText(newText);
+    localStorage.setItem('textutil', newText);
+    props.showAlert('Case Toggled!', 'success');
+  };
+
+  const handleReplace = () => {
+    let existing_text = prompt('Enter which word to replace: ');
+    let replaced_text = prompt('Enter new word');
+    let newText = text.replaceAll(existing_text, replaced_text);
+    setText(newText);
+    localStorage.setItem('textutil', newText);
+    props.showAlert(
+      `${existing_text} is replaced with ${replaced_text}`,
+      'success'
+    );
+  };
+
+  const handleClearClick = () => {
+    setText('');
+    props.showAlert('Text Cleared!', 'success');
+  };
+
+  const undo = () => {
+    let itm = localStorage.getItem('textutil');
+    setText(itm);
+    props.showAlert('Text Retrieved!', 'success');
+  };
+
+  const speak = () => {
+    let msg = new SpeechSynthesisUtterance();
+    msg.text = text;
+    window.speechSynthesis.speak(msg);
+  };
+
+  const handleCopy = () => {
+    let textBox = document.getElementById('myBox');
+    textBox.select();
+    navigator.clipboard.writeText(textBox.value);
+    props.showAlert('Text copied to clipboard!', 'success');
+  };
+
+  const handleExtraSpaces = () => {
+    let newText = text.split(/[ ]+/).join(' ');
+    setText(newText);
+    localStorage.setItem('textutil', newText);
+    props.showAlert('Extra Spaces removed!', 'success');
+  };
+
+  return (
+    <>
+      <div
+        className="container my-3"
+        style={{ color: props.mode === 'light' ? 'black' : 'white' }}
+      >
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            id="myBox"
+            rows={8}
+            style={{
+              backgroundColor:
+                props.mode === 'dark' ? 'rgb(108 170 232)' : 'white',
+              color: props.mode === 'light' ? 'black' : 'white',
+            }}
+          ></textarea>
+        </div>
+        <button
+          className={`btn btn-outline-${
+            props.mode === 'light' ? 'dark' : 'light'
+          }`}
+          onClick={handleUpClick}
+        >
+          Uppercase
+        </button>
+        <button
+          className={`btn btn-outline-${
+            props.mode === 'light' ? 'dark' : 'light'
+          } mx-2`}
+          onClick={handleLowClick}
+        >
+          Lowercase
+        </button>
+        <button
+          className={`btn btn-outline-${
+            props.mode === 'light' ? 'dark' : 'light'
+          } mx-2`}
+          onClick={handleCapitalize}
+        >
+          Capitalize
+        </button>
+        <button
+          className={`btn btn-outline-${
+            props.mode === 'light' ? 'dark' : 'light'
+          } mx-2`}
+          onClick={handleToggleCase}
+        >
+          Toggle Case
+        </button>
+        <button
+          className={`btn btn-outline-${
+            props.mode === 'light' ? 'dark' : 'light'
+          } mx-2`}
+          onClick={handleReplace}
+        >
+          Replace
+        </button>
+        <button
+          className={`btn btn-outline-${
+            props.mode === 'light' ? 'dark' : 'light'
+          } mx-2`}
+          onClick={handleClearClick}
+        >
+          Clear
+        </button>
+        <button
+          className={`btn btn-outline-${
+            props.mode === 'light' ? 'dark' : 'light'
+          } mx-2`}
+          onClick={undo}
+        >
+          Undo
+        </button>
+        <button
+          onClick={speak}
+          className={`btn btn-outline-${
+            props.mode === 'light' ? 'dark' : 'light'
+          } mx-2`}
+        >
+          Speak
+        </button>
+        <button
+          onClick={handleCopy}
+          className={`btn btn-outline-${
+            props.mode === 'light' ? 'dark' : 'light'
+          } mx-2`}
+        >
+          Copy
+        </button>
+        <button
+          onClick={handleExtraSpaces}
+          className={`btn btn-outline-${
+            props.mode === 'light' ? 'dark' : 'light'
+          } mx-2`}
+        >
+          Remove Extra Spaces
+        </button>
+      </div>
+      <div
+        className="container my-3"
+        style={{ color: props.mode === 'light' ? 'black' : 'white' }}
+      >
+        <h2>Your Text Summary</h2>
+        <p>
+          <b>{text.trim() === '' ? 0 : text.trim().split(' ').length}</b> Words
+          and <b>{text.length}</b> Characters
+        </p>
+        <p>
+          <b>
+            {text === ''
+              ? text
+                  .trim()
+                  .split('.')
+                  .filter((text) => text !== '').length
+              : text.split('.').length - 1}{' '}
+          </b>
+          No. of sentences
+        </p>
+        <p>
+          <b>
+            {0.008 * (text.trim() === '' ? 0 : text.trim().split(' ').length)}
+          </b>{' '}
+          Minutes to read
+        </p>
+      </div>
+      <div
+        className="container my-3"
+        style={{ color: props.mode === 'light' ? 'black' : 'white' }}
+      >
+        <h2>Preview</h2>
+        <p>{text.length > 0 ? text : 'Enter some text to preview it here.'}</p>
+      </div>
+    </>
+  );
+};
+
+export default TextForm;
